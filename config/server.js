4cf188e062b6671b4310a8795d3b4e43a8845035
Richard <kasonde.nsama@gmail.com>
2021-09-10 10:59:50 +0200

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'caaa50481e825e17bbeddca8de637fc6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1260f7c5-d851-434a-85b2-f44fe35577d5'
+};
